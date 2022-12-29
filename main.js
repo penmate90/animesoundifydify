e87f03dea02357d8d1b8dc32a8ae786026ddf348
penmate90 <84581597+penmate90@users.noreply.github.com>
2022-12-29 15:42:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+nem=null;
+
 function start() {
     navigator.mediaDevices.getUserMedia({audio: true});
 
@@ -10,9 +12,31 @@ function redy() {
 }
 
 function gotresult(results, error) {
-    if (error) {
-      console.error(error);  
-    } else {
-        
-    }
+    r=Math.floor(Math.random()*255)
+    g=Math.floor(Math.random()*255)
+    b=Math.floor(Math.random()*255)
+      console.log(error);  
+      nem=error[0].label;
+      conf=Math.floor(error[0].confidence*100)+"%"
+      document.getElementById("sound_info").style.color=(r,g,b);
+      if (nem=="woof") {
+          document.getElementById("sound_info").innerHTML="I can hear-Dog "+conf;
+          document.getElementById("img").src="dog.gif"
+      } else if(nem=="mow") {
+        document.getElementById("sound_info").innerHTML="I can hear-cat "+conf;
+        document.getElementById("img").src="cat.gif"
+      }
+      else if(nem=="MOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo") {
+        document.getElementById("sound_info").innerHTML="I can hear-Cow "+conf;
+        document.getElementById("img").src="cow.gif"
+      }
+      else if(nem=="ROAARRRRR") {
+        document.getElementById("sound_info").innerHTML="I can hear-Lion "+conf;
+        document.getElementById("img").src="lio.gif"
+      }
+      else{
+        document.getElementById("sound_info").innerHTML="I can hear-Background noise "+conf;
+        document.getElementById("img").src="nois.gif"
+      }
+   
 }
